ecebf4830a75e02ba58cf09eb9beaea153e65b8c
kael <macabalimicooz@gmail.com>
2025-12-23 09:27:50 +0800

    Update versionName to 1.11.4 and versionCode to 162

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ fun runCommand(command: String): String {
 }
 
 @Suppress("PropertyName")
-val _versionName = "1.11.2"
+val _versionName = "1.11.4"
 val betaCount by lazy {
     val betaTags = runCommand("git tag -l --sort=refname v${_versionName}-b*")
 
@@ -50,7 +50,7 @@ val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 android {
     defaultConfig {
         applicationId = "me.kaelsh.kiodex"
-        versionCode = 160
+        versionCode = 162
         versionName = _versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
